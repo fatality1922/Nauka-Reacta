5f1549928b7e0aa6fa5ce6cabb31308c0dbf0100
Jan Kowalski <fatality1922@gmail.com>
2021-01-06 16:42:14 +0100

    update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
 
 class Card extends React.Component {
+
+    static propTypes = {
+        key: PropTypes.string,
+        title: PropTypes.string.isRequired,
+    }
+
     render() {
         return (
             <div className={styles.component}>
-                <h3>{this.props.column.key}</h3>
-                <h3>{this.props.column.title}</h3>
+                <h3>{this.props.key}</h3>
+                <h3>{this.props.title}</h3>
             </div>
         );
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import { settings } from '../../data/dataStore';
 import Creator from '../Creator/Creator.js';
+import ReactHtmlParser from 'react-html-parser';
+
 
 class List extends React.Component {
   state = {
@@ -42,8 +44,8 @@ class List extends React.Component {
         <Hero titleText={this.props.title} backgroudImage={this.props.image} />
 
         <div className={styles.description}>
-          {this.props.description}
-          {/* {ReactHtmlParser(this.props.description)}  dlaczego nie dziala?*/}
+          {/* {this.props.description} */}
+          {ReactHtmlParser(this.props.description)}  
         </div>
 
         <div className={styles.columns}>
