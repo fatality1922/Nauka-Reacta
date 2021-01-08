55d21a50811656212863ad868c475dfaf3b44438
Jan Kowalski <fatality1922@gmail.com>
2021-01-08 17:20:14 +0100

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js';
-import { pageContents, listData } from '../../data/dataStore';
+//import List from '../List/List.js';
 //import Creator from '../Creator/Creator';
 
+
 class App extends React.Component {
-
-  // addListColumns(title){
-  //   this.setState(state => (
-  //     {
-  //       listData: [
-  //         ...state.listData,
-  //         {
-  //           key: state.listData.length ? state.listData[state.listData.length-1].key+1 : 0, //??
-  //           title,
-  //            icon: 'list-alt',
-  //           /cards: [],
-  //         }
-  //       ]
-  //     }
-  //   ));
-  // } robic to?
-
-
+  static propTypes = {
+    title: this.propTypes.node,
+    subtitle: this.propTypes.node,
+  }
+  
   render() {
+    const { title, subtitle } = this.props;
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}> {pageContents.subtitle}</h2>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {/*
         <List {...listData} />
-        {/* <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />  i to?? */}
+        */}
       </main>
     );
   }
